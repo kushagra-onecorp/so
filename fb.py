2b1febd205d35248a4aa2a0c72cb8e4393937907
kushagra-oncorp <kushagra@onecorp.co.in>
2022-08-24 11:02:35 +0530

    added post 2 posts

--- fb.py
+++ fb.py
@@ -1,9 +1,10 @@
+import json
 import requests
 
 # Access Token from Facebook Graph API
-facebook_access_token="EAAUFRLCbo1MBAG36jYRZAXglCkumUbZBWmISO3GojsZA81ZAazEFkX6AJcqGOaxTStAkZAM8YAL2aO05RatdQBRUtiLbrXl8kICy30DqiYiL2TcEmBEoa9ODnSaBstFZCimNswHuvlPeplqjjQNig7jDaOw2bzyjuzF2DcSFZC04gfyPxStSA4kwrPbAeGLpjToYWIfEnv0kN0Q4dwO2uMa"
+facebook_access_token="EAAUFRLCbo1MBAAOGRYE9Uju7W9iUZBScmL1CaBvO96JyJqX2L5ajM7h4HxpOKtlcxOZC6wRqedPSPiZC9GhEeL4et61NNjXNuUupLUhpD4X4tt0thyFLZCol9d9I5OMfr0UFxhjaZAZBYlxEUACgoq00mMYQjGWkLenujQw9S0Vo0wz3lMkZClA"
 # Facebook Page ID
-facebook_page_id='103995469037355'
+facebook_page_id='102068252597979'
 # Post Title
 post_title='Post Title'
 # Post Description
@@ -15,15 +16,37 @@ post_text=f"""
 """
 # URL of the image to post
 image_url='https://wallpaperaccess.com/full/1398313.jpg'
+image2='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+facebook_photo_upload_url=f'https://graph.facebook.com/v14.0/{facebook_page_id}/photos'
+facebook_upload_body1={
+        'url':image_url,
+        'published':'false',
+        'access_token':facebook_access_token
+        }
+facebook_upload_body2={
+        'url':image2,
+        'published':'false',
+        'access_token':facebook_access_token
+        }
+print(facebook_photo_upload_url)
+print(facebook_upload_body1)
+print(facebook_upload_body2)
+facebook_image1= requests.post(facebook_photo_upload_url,data=facebook_upload_body1)
+print(facebook_image1.text)
+facebook_image2= requests.post(facebook_photo_upload_url,data=facebook_upload_body2)
+print(facebook_image2.text)
 # Facebook API Request URL
-facebook_request_url = f'https://graph.facebook.com/{facebook_page_id}/photos'
+facebook_request_url = f'https://graph.facebook.com/{facebook_page_id}/feed'
 # Facebook Request Payload
 facebook_post_payload = {
 'message':post_text,
-'url': image_url,
+'attached_media':json.dumps([
+        {'media_fbid':facebook_image1.json()['id']},
+        {'media_fbid':facebook_image2.json()['id']}]),
 'access_token': facebook_access_token
 }
-
+print(facebook_request_url)
+print(facebook_post_payload)
 #Send POST request to Facebook API
 facebook_resonse = requests.post(facebook_request_url,data=facebook_post_payload)
 
--- insta.py
+++ insta.py
@@ -1,11 +1,12 @@
 import requests
 
 # Access Token from Facebook Graph API
-instragram_access_token="EAAUFRLCbo1MBAG36jYRZAXglCkumUbZBWmISO3GojsZA81ZAazEFkX6AJcqGOaxTStAkZAM8YAL2aO05RatdQBRUtiLbrXl8kICy30DqiYiL2TcEmBEoa9ODnSaBstFZCimNswHuvlPeplqjjQNig7jDaOw2bzyjuzF2DcSFZC04gfyPxStSA4kwrPbAeGLpjToYWIfEnv0kN0Q4dwO2uMa"
+instragram_access_token="EAAUFRLCbo1MBAAOGRYE9Uju7W9iUZBScmL1CaBvO96JyJqX2L5ajM7h4HxpOKtlcxOZC6wRqedPSPiZC9GhEeL4et61NNjXNuUupLUhpD4X4tt0thyFLZCol9d9I5OMfr0UFxhjaZAZBYlxEUACgoq00mMYQjGWkLenujQw9S0Vo0wz3lMkZClA"
 # Instagram User ID
 instagram_user_id='17841453883347404'
 # URL of the image to post
 image_url='https://wallpaperaccess.com/full/1398313.jpg'
+image2='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 # Post Title
 post_title='Post Title'
 # Post Description
@@ -18,23 +19,41 @@ post_text=f"""
 # API Request URL to upload the image
 instagram_media_request_url = f'https://graph.facebook.com/v10.0/{instagram_user_id}/media'
 # Request payload for media request
-instagram_media_payload = {
+instagram_media_payload1 = {
 'image_url': image_url,
 'caption':post_text,
+'is_carousel_item':'true',
+'access_token': instragram_access_token
+}
+instagram_media_payload2 = {
+'image_url': image2,
+'caption':post_text,
+'is_carousel_item':'true',
 'access_token': instragram_access_token
 }
 #Send POST request to media url
-instagram_media_response = requests.post(instagram_media_request_url,data=instagram_media_payload)
-print(instagram_media_response.json())
+instagram_media_response1 = requests.post(instagram_media_request_url,data=instagram_media_payload1)
+print(instagram_media_response1.json())
+instagram_media_response2 = requests.post(instagram_media_request_url,data=instagram_media_payload2)
+print(instagram_media_response2.json())
 
+
+instagram_media_payload3 = {
+'media_type': 'CAROUSEL',
+'caption':post_text,
+'children':','.join([instagram_media_response1.json()['id'],instagram_media_response2.json()['id']]),
+'access_token': instragram_access_token
+}
+instagram_media_response3 = requests.post(instagram_media_request_url,data=instagram_media_payload3)
+print(instagram_media_response3.json())
 # Instagram Graph API publish URL
 instagram_publish_url=f'https://graph.facebook.com/v10.0/{instagram_user_id}/media_publish'
 # Payload for publish request
 instagram_publish_payload = {
-'creation_id':instagram_media_response.json()['id'],
+'creation_id':instagram_media_response3.json()['id'],
 'access_token': instragram_access_token
 }
 
 #Send POST request to publish url
 instagram_publish_response = requests.post(instagram_publish_url,data=instagram_publish_payload)
-print(instagram_media_response.json())
+print(instagram_media_response1.json())
